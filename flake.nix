d0693312438758d541038388c18ceddfae8b6c34
Denis Goncharenko <denjer@mail.ru>
2021-09-15 22:14:39 +0200

    feat: init

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    devshell.url = "github:numtide/devshell";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, devshell, flake-utils, nixpkgs }:
+    let overlay = _: prev: { nodejs = prev.nodejs-14_x; };
+
+    in flake-utils.lib.simpleFlake {
+      inherit self nixpkgs;
+
+      name = "@dddenis/firestore-fp";
+      preOverlays = [ devshell.overlay overlay ];
+      systems = flake-utils.lib.defaultSystems;
+
+      shell = { pkgs }:
+        pkgs.devshell.mkShell {
+          motd = "";
+          packages = with pkgs; [ nodejs yarn ];
+        };
+    };
+}
